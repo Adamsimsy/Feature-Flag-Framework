44ca9e1f1695f4a5a17190189abb9a14eda252ff
adamsimsy <adamsimsy@gmail.com>
2020-07-26 20:59:14 +1000

    Tidying of javascript library

--- src/FeatureFlagFramework.Example.Aspnetcore/wwwroot/js/FeatureFlagFramework.js
+++ src/FeatureFlagFramework.Example.Aspnetcore/wwwroot/js/FeatureFlagFramework.js
@@ -1,32 +1,6 @@
-﻿var getJSON = function (url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function () {
-        var status = xhr.status;
-        if (status === 200) {
-            callback(null, xhr.response);
-        } else {
-            callback(status, xhr.response);
-        }
-    };
-    xhr.send();
-};
-
-var featureFlagFrameworkToggleCollection;
-
-getJSON('https://jsontoggler.blob.core.windows.net/examples/flags.json',
-    function (err, data) {
-        if (err !== null) {
-            alert('Something went wrong: ' + err);
-        } else {
-            featureFlagFrameworkToggleCollection = data;
-        }
-    });
-
-var featureFlagFramework = {
+﻿var featureFlagFramework = {
     evaluate: function (name, defaultValue) {
-        var feature = featureFlagFrameworkToggleCollection.features.find(el => el.name === name);
+        var feature = featureFlagFramework.toggleCollection.features.find(el => el.name === name);
 
         if (feature === undefined) {
             return defaultValue;
@@ -34,5 +8,29 @@ var featureFlagFramework = {
         else {
             return feature.enabled;
         }
-    }
+    },
+    getJson : function (url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function () {
+            var status = xhr.status;
+            if (status === 200) {
+                callback(null, xhr.response);
+            } else {
+                callback(status, xhr.response);
+            }
+        };
+        xhr.send();
+    },
+    toggleCollection : null
 }
+
+featureFlagFramework.getJson('https://jsontoggler.blob.core.windows.net/examples/flags.json',
+    function (err, data) {
+        if (err !== null) {
+            alert('Something went wrong: ' + err);
+        } else {
+            featureFlagFramework.toggleCollection = data;
+        }
+    });
