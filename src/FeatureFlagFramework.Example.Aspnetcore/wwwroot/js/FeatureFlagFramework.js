4abe6a2106bfbdeea575a6288a819a8bf97c1988
adamsimsy <adamsimsy@gmail.com>
2020-07-26 20:53:39 +1000

    Added initial version of featureFlagFramework.js

---
+++ src/FeatureFlagFramework.Example.Aspnetcore/wwwroot/js/FeatureFlagFramework.js
@@ -0,0 +1,38 @@
+﻿var getJSON = function (url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
+
+var featureFlagFrameworkToggleCollection;
+
+getJSON('https://jsontoggler.blob.core.windows.net/examples/flags.json',
+    function (err, data) {
+        if (err !== null) {
+            alert('Something went wrong: ' + err);
+        } else {
+            featureFlagFrameworkToggleCollection = data;
+        }
+    });
+
+var featureFlagFramework = {
+    evaluate: function (name, defaultValue) {
+        var feature = featureFlagFrameworkToggleCollection.features.find(el => el.name === name);
+
+        if (feature === undefined) {
+            return defaultValue;
+        }
+        else {
+            return feature.enabled;
+        }
+    }
+}
